10ab76c093e799983e7163c6e0989c190ad2adc7
Sannio82 <susanna.johansson@iths.se>
2021-12-07 10:59:08 +0100

    Draw works

--- TicTacToe/PlayGameViewController.swift
+++ TicTacToe/PlayGameViewController.swift
@@ -2,10 +2,9 @@
 import UIKit
 
 class PlayGameViewController: UIViewController {
-
+    
     @IBOutlet weak var buttonPlayAgain: UIButton!
     @IBOutlet weak var label: UILabel!
-    var count = 1
     var activePlayer = 1
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     var gameIsActive = true
@@ -13,59 +12,64 @@ class PlayGameViewController: UIViewController {
     var player1 = ""
     let segueToFirstVC = "segueToFirstVC"
     
-   override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-       label.text = "You chose \(player1)"
-   }
-
+        label.text = "You chose \(player1)"
+        if player1 == "X" {
+            activePlayer = 1
+        }else{
+            activePlayer = 2
+        }
+    }
+    
     @IBAction func buttonPressed(_ sender: AnyObject) {
         
         buttonPlayAgain.isHidden = true
         label.isHidden = true
         if(gameState[(sender as AnyObject).tag-1] == 0 && gameIsActive == true) {
             gameState[(sender as AnyObject).tag-1] = activePlayer
-                if (activePlayer == 1) {
-                    (sender as AnyObject).setImage(UIImage(named: "TictactoeXEdited"), for: UIControl.State())
-                    activePlayer = 2
-                    print(activePlayer)
-                }else{
-                    (sender as AnyObject).setImage(UIImage(named: "TictactoeOEdited"), for:
-                        UIControl.State())
-                    activePlayer = 1
-                    print(activePlayer)
-                }
-
-    }
+            if (activePlayer == 1) {
+                (sender as AnyObject).setImage(UIImage(named: "TictactoeXEdited"), for: UIControl.State())
+                activePlayer = 2
+                
+            }else{
+                (sender as AnyObject).setImage(UIImage(named: "TictactoeOEdited"), for:
+                                                UIControl.State())
+                activePlayer = 1
+                
+            }
+            
+        }
         
         for combination in winningCombinations {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
                 gameIsActive = false
-                
-                if gameState[combination[0]] == 1 {
-                    label.text = ("X won!")
-                }else{
-                label.text = ("O won!")
-            }
-                
-                if gameIsActive == true {
-                    for i in gameState {
-                        count = i*count
-                    }
-                    if count != 0 {
-                     label.text = "It was a draw!"
-                        label.isHidden = false
-                        buttonPlayAgain.isHidden = false
-                    }
-                }
-                
                 buttonPlayAgain.isHidden = false
                 label.isHidden = false
-    
+                if gameState[combination[0]] == 1 {
+                    label.text = ("X won!")
+                    
+                }else{
+                    label.text = ("O won!")
+                }
+            }
+            }
+        var count = 0
+        if gameIsActive == true {
+            for i in gameState {
+                if i != 0 {
+                    count += 1
+                }
+            }
+            if count == 9 {
+                label.text = "Draw!"
+                label.isHidden = false
+                buttonPlayAgain.isHidden = false
             }
         }
         
         
-        }
+    }
     
     @IBAction func playAgain(_ sender: Any) {
         
@@ -76,23 +80,24 @@ class PlayGameViewController: UIViewController {
         
         if segue.identifier == segueToFirstVC {
             reset()
-    }
-    
-    func reset() {
-        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        gameIsActive = true
-        activePlayer = 1
-        buttonPlayAgain.isHidden = true
-        label.isHidden = true
-        
-        for i in 1...9 {
-            let button = view.viewWithTag(i) as! UIButton
-            button.setImage(nil, for: UIControl.State())
         }
+        
+        func reset() {
+            gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            gameIsActive = true
+            activePlayer = 1
+            buttonPlayAgain.isHidden = true
+            label.isHidden = true
+            
+            for i in 1...9 {
+                let button = view.viewWithTag(i) as! UIButton
+                button.setImage(nil, for: UIControl.State())
+            }
         }
     }
 }
-    
+
+
 
 
 
